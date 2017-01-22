5bb412d6bdb86d1d2604419de456516163b7cd95
KAIJIE Zhan <ceres0503@icloud.com>
2017-01-22 02:20:13 -0500

    complete extra exercise

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -11,12 +11,13 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+	gradJ = 1/(2*m) * 2 * (X'*X*theta - X'*y);
+ 	theta = theta - alpha * gradJ;
 
 
 
@@ -29,7 +30,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
